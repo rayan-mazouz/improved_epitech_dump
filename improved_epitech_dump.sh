77ed8a7bb4991b571928a4a3697be465d7a51294
Louis Dupraz <114499642+LouisDupraz@users.noreply.github.com>
2022-09-29 07:38:57 -0400

    Fixed error

--- improved_epitech_dump.sh
+++ improved_epitech_dump.sh
@@ -20,7 +20,8 @@ git clone https://github.com/rayan-mazouz/improved_epitech_dump.git
 cd improved_epitech_dump/
 
 # Creating /etc/yum.repos.d/ if it does not exist
-if [ ls /etc | grep "yum.repos.d" == "" ]; then
+yr = /etc/yum.repos.d
+if [ -d "$yr" ] ; then
 	mkdir /etc/yum.repos.d
 fi
 
