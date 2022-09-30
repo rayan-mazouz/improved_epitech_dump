18e57602ee1fb7fc4492abe13f28716b208a3d40
Louis Dupraz <114499642+LouisDupraz@users.noreply.github.com>
2022-09-30 14:45:48 +0200

    Changed CSC download to git clone

--- improved_epitech_dump.sh
+++ improved_epitech_dump.sh
@@ -83,9 +83,9 @@ cd ..
 rm -rf improved_epitech_dump
 
 # Create folders for the coding style checkers and download it.
-mkdir ~/CodingStyleChecker
-cd ~/CodingStyleChecker
-wget https://raw.githubusercontent.com/Epitech/coding-style-checker/main/coding-style.sh; chmod +x coding-style.sh
+cd ~
+git clone https://github.com/Epitech/coding-style-checker.git
+cd ./coding-style-checker
 mkdir ./Outputs
 cd /tmp
 
